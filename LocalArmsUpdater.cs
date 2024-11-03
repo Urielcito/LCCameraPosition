9ef565dda54dd037f6dd1e3929d13225fb3d4bb1
Uriel Larrea <kuroshiromugen@gmail.com>
2024-11-03 05:25:34 -0300

    arms now follow player, fov changes, and player model shrinks

---
+++ LocalArmsUpdater.cs
@@ -0,0 +1,71 @@
+﻿using BepInEx.Logging;
+using GameNetcodeStuff;
+using LCCameraPosition.Patches;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace LCCameraPosition
+{
+    [DefaultExecutionOrder(-1)]
+    internal class LocalArmsUpdater : MonoBehaviour
+    {
+        internal PlayerControllerB plrCntrB;
+
+        private GameObject offsetBuild;
+
+        private Vector3 org;
+        private Vector3 val;
+
+        internal ManualLogSource mls;
+
+        private void Start()
+        {
+            mls = BepInEx.Logging.Logger.CreateLogSource("LocalArmsUpdater.kuroshiromugen");
+            val = Vector3.zero;
+        }
+
+        private void LateUpdate()
+        {
+            if (plrCntrB == null)
+            {
+                return;
+            }
+            if (offsetBuild == null || offsetBuild.transform.parent == null)
+            {
+                if (offsetBuild == null)
+                {
+                    offsetBuild = new GameObject("LocalOffset");
+                }
+                offsetBuild.transform.parent = plrCntrB.localArmsTransform.parent;
+                org = offsetBuild.transform.parent.localPosition;
+                val = org;
+                return;
+            }
+            if(!plrCntrB.inTerminalMenu && !plrCntrB.inSpecialInteractAnimation)
+            {
+                var k = Keyboard.current;
+                if (k.upArrowKey.isPressed)
+                {
+                    val.y += 0.01f;
+                }
+                if (k.downArrowKey.isPressed)
+                {
+                    val.y -= 0.01f;
+                }
+                if (k.rightArrowKey.isPressed)
+                {
+                    val.x += 0.01f;
+                }
+                if (k.leftArrowKey.isPressed)
+                {
+                    val.x -= 0.01f;
+                }
+                if (k.leftShiftKey.isPressed && k.numpad0Key.isPressed)
+                {
+                    val = org;
+                }
+            }
+            offsetBuild.transform.parent.localPosition = val;
+        }
+    }
+}
--- Patches/CameraPatch.cs
+++ Patches/CameraPatch.cs
@@ -4,6 +4,7 @@ using BepInEx.Logging;
 using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 namespace LCCameraPosition.Patches
 {
@@ -11,7 +12,12 @@ namespace LCCameraPosition.Patches
     public class CameraPatch : MonoBehaviour
     {
         private PlayerControllerB localPlayer;
+        private LocalArmsUpdater localArmsUpdater;
         private UnityEngine.Vector3 cameraOffset = UnityEngine.Vector3.zero;
+
+        internal ManualLogSource mls;
+        public static float zoomFov = 0f;
+        public static UnityEngine.Vector3 playerScale;
 
         [HarmonyPatch("Awake")]
         [HarmonyPostfix]
@@ -20,35 +26,61 @@ namespace LCCameraPosition.Patches
             ((UnityEngine.Component)__instance).gameObject.AddComponent<CameraPatch>();
         }
 
+        private void Start()
+        {
+            mls = BepInEx.Logging.Logger.CreateLogSource("CameraPatch.kuroshiromugen");
+            if ((Object)(object)localArmsUpdater == (Object)null)
+            {
+                localArmsUpdater = ((Component)this).gameObject.AddComponent<LocalArmsUpdater>();
+            }
+        }
         private void Update()
         {
-            localPlayer = StartOfRound.Instance.localPlayerController;
+            if((Object)(object)localPlayer == (Object)null)
+            {
+                localPlayer = StartOfRound.Instance.localPlayerController;
+                zoomFov = 68f;
+                playerScale = localPlayer.thisPlayerBody.transform.localScale;
+                localArmsUpdater.plrCntrB = localPlayer;
+            }
+
             var k = Keyboard.current;
             if (k.upArrowKey.isPressed)
             {
                 cameraOffset.y += 0.01f;
+                playerScale.y += 0.01f;
+                zoomFov += 0.1f;
             }
             if (k.downArrowKey.isPressed)
             {
                 cameraOffset.y -= 0.01f;
+                playerScale.y -= 0.01f;
+                zoomFov -= 0.1f;
             }
-            if (k.rightArrowKey.isPressed)
+            if(k.leftShiftKey.isPressed && k.numpad0Key.isPressed)
             {
-                cameraOffset.x += 0.01f;
-            }
-            if (k.leftArrowKey.isPressed)
-            {
-                cameraOffset.x -= 0.01f;
+                cameraOffset = UnityEngine.Vector3.zero;
             }
 
-            if (!localPlayer.isPlayerDead && !localPlayer.inSpecialInteractAnimation && !localPlayer.inTerminalMenu)
+            if (!localPlayer.isPlayerDead && !localPlayer.inTerminalMenu)
             {
-                ((Component)localPlayer.gameplayCamera).transform.localPosition = cameraOffset;
+                localPlayer.gameplayCamera.transform.localPosition = cameraOffset;
+                if (localPlayer.gameplayCamera.fieldOfView != zoomFov)
+                    mls.LogInfo("FOV is " + zoomFov);
+                localPlayer.gameplayCamera.fieldOfView = zoomFov;
+
+                localPlayer.thisPlayerBody.transform.localScale = playerScale;
             }
             else
             {
-                ((Component)localPlayer.gameplayCamera).transform.localPosition = UnityEngine.Vector3.zero;
+                localPlayer.gameplayCamera.transform.localPosition = UnityEngine.Vector3.zero;
+                localPlayer.gameplayCamera.fieldOfView = 68f;
             }
+        }
+
+        public UnityEngine.Vector3 getCameraOffset()
+        {
+            return this.cameraOffset;
         }
     }
 }
